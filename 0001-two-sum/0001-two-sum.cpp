b36e632d6b4d6e2ac1030a4a3109fca721642b28
developeraminul <aminul.cse.cuet@gmail.com>
2022-10-10 23:31:31 +0600

    Time: 38 ms (47.10%), Space: 12.1 MB (8.71%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,30 +1,39 @@
 #include <vector>
 using namespace std;
-class Solution {
-public:
-unordered_map<int, int> map;
-	vector<int> twoSum(vector<int>& nums, int target) {
-		map.clear();
-		int first;
-		int second;
+class Solution
+{
+    public:
+        unordered_map<int, int> map;
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        map.clear();
+        int first;
+        int second;
         int x;
         vector<int>::iterator it;
-		for (it = nums.begin(); it != nums.end(); it++) {
-			map.insert(pair<int, int>(*it, it - nums.begin()));
-            //intf("key: %d Value:%d\n", *it, it - nums.begin());
-		}
+        for (it = nums.begin(); it != nums.end(); it++)
+        {
+            map.insert(pair<int, int> (*it, it - nums.begin()));
+           	//intf("key: %d Value:%d\n", *it, it - nums.begin());
+        }
 
-		unordered_map<int, int>::iterator mapIt;
+        unordered_map<int, int>::iterator mapIt;
 
-		for (auto it = nums.begin(); it != nums.end(); it++) {
+        for (auto it = nums.begin(); it != nums.end(); it++)
+        {
             x = target - *it;
-			if (map.find(x) != map.end()) {
-				first = it - nums.begin();
-				second = map.find(target - *it)->second;
-                if(first != second)
-					return { first, second};
-			}
-		}
-		return { 0, 1 };
-	}
+            if (map.find(x) != map.end())
+            {
+                first = it - nums.begin();
+                second = map.find(target - *it)->second;
+                if (first != second)
+                    return {
+                        first,
+                        second
+                    };
+            }
+        }
+        return { 0,
+            1 };
+    }
 };
